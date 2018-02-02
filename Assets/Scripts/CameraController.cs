83555a3b0894baa33cd49abfc09722619df0e188
Mojtaba Hafezi <mojtaba.hafezi@hotmail.de>
2018-02-02 01:36:43 +0000

    Project initialisation Created player charachter with different animation and basic movement Created prefab template for first tile with physics Added a script to the camera to follow player on movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//make the camera follow the player
+public class CameraController : MonoBehaviour
+{
+	private GameObject player;
+	//difference between camera and player transform positions
+	private Vector3 offset;
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	//guaranteed after all items have been processed in update -> player has already moved
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	//Components
+	private Rigidbody2D rigidBody;
+	private Animator animator;
+
+	//Attributes
+	public float horizontalSpeed;
+	public float verticalSpeed;
+	private float currentSpeed = 3;
+	private float currentSpeedNegative = -3;
+	private bool facingRight = true;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		facingRight = true;
+	}
+
+	//fixedupdate is called just before performing physic calculations -> movement comes here (no Time.deltatime)
+	void FixedUpdate ()
+	{
+		float newSpeed;
+
+		//Keys are set in the InputManager by default
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//set speed for walking animation - mathf.abs for positive value only
+		animator.SetFloat ("speed", Mathf.Abs (moveHorizontal));
+
+		//check for maxSpeed - can be upgraded later
+		newSpeed = moveHorizontal * horizontalSpeed;
+		if (newSpeed >= currentSpeed)
+			newSpeed = currentSpeed;
+		else if (newSpeed <= currentSpeedNegative)
+			newSpeed = currentSpeedNegative;
+
+		//add forces to the rigidBody2D for movement
+		rigidBody.AddForce (new Vector2 (newSpeed, moveVertical * verticalSpeed));
+
+		//If we moving left and facing right -> flip, if we move right and not facing right -> flip
+		if (moveHorizontal > 0 && !facingRight)
+			FlipSprite ();
+		else if (moveHorizontal < 0 && facingRight)
+			FlipSprite ();
+	}
+
+	//Easy way to flip sprite for 180 degree
+	void FlipSprite ()
+	{
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+
+
+
+	public float GetCurrentSpeed ()
+	{
+		return currentSpeed;
+	}
+
+	public void SetCurrentSpeed (float newSpeed)
+	{
+		currentSpeed = newSpeed;
+	}
+
+
+
+
+}
